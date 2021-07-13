c8d46091dd49b9e8f0c05c35bb676b2da3f95330
Juan Ramirez <juanramirezcastaneda@gmail.com>
2021-07-13 09:05:08 -0500

    Add default project

---
+++ Consecutive.js
@@ -0,0 +1,11 @@
+// Todo: return the adjacent elements
+// bonus -> avoid sorting and use recursion
+// e.g: [100, 4, 200, 1, 3, 2] -> [1, 2, 3, 4, 100, 200] and return 4 adjacent elements
+// O(n) complexity
+
+var findConsecutive = (input) => {
+    console.log('input: ', input);
+};
+
+var input = [100, 4, 200, 1, 3, 2];
+findConsecutive(input);
---
+++ Duplicates.js
@@ -0,0 +1,10 @@
+// Todo: remove duplicates by reference and return length 
+// e.g: [1, 1, 1, 2, 3, 3, 4] -> [1, 2, 3, 4] and return 4
+// O(n) complexity
+
+var removeDuplicates = (input) => {
+    console.log('input: ', input);
+};
+
+var input = [1, 1, 2, 3, 4, 4];
+removeDuplicates(input);
