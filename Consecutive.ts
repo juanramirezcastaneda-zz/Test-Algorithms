ac872fb60f03dc3b765e1eb075bd55242744675f
Juan Ramirez <juanramirezcastaneda@gmail.com>
2021-07-19 17:18:33 -0500

    Solve consecutive

--- Consecutive.ts
+++ Consecutive.ts
@@ -3,9 +3,22 @@
 // e.g: [100, 4, 200, 1, 3, 2] -> [1, 2, 3, 4, 100, 200] and return 4 adjacent elements
 // O(n) complexity
 
-var findConsecutive = (input) => {
-    console.log('input: ', input);
+var findConsecutive = (input: number[]) => {
+    const sortedArray = input.sort((a, b) => a - b);
+    const res = [];
+ 
+    for (let i = 0; i < sortedArray.length; i++) {
+        const element = sortedArray[i];
+        const followingElement = sortedArray[i + 1];
+        
+        if(followingElement - element === 1){
+            res.push(element);
+            res.push(followingElement);
+            i++;
+        }
+    }
+    return res;
 };
 
 var input = [100, 4, 200, 1, 3, 2];
-findConsecutive(input);
+console.log(findConsecutive(input));
